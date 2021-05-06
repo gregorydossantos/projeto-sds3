1598a05e2d6a72fc65dcadb9ff1186a8c61cc1fa
Gregory Castezana <gregescribano@gmail.com>
2021-05-05 21:43:24 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/gregorydossantos" target="_blank" rel="noreferrer">Gregory Castezana</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+      Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/gregcastezana" target="_blank" rel="noreferrer">@gregcastezana</a></small></p>
+             </div>
+        </footer>
+    );
+  }
+  
+  export default Footer;
